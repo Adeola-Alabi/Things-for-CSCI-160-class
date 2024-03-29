2c4c6840269607580324e5d80343b9964396f76e
Adeola-Alabi <Erron.smith2020@gmail.com>
2024-03-28 21:11:50 -0700

    committing everythign I worked on Today

--- Prog7/Prog7part1.py
+++ Prog7/Prog7part1.py
@@ -4,3 +4,11 @@ class_name = input("Class Name: ")
 if class_name != None:
     grade = input("Grade: ")
     credits = input("Credits: ")
+    f = open(file_name, "a")
+    while class_name != "":
+        class_info = (class_name + ":" + grade + ":" + credits + " ")
+        f.write(class_info)
+        class_name = input("Class Name: ")
+        grade = input("Grade: ")
+        credits = input("Credits: ")
+f.close   
--- Prog7part1.py
+++
@@ -1,6 +0,0 @@
-file_name = input("File Name: ")
-class_name = input("Class Name: ")
-
-if class_name != None:
-    grade = input("Grade: ")
-    credits = input("Credits: ")
