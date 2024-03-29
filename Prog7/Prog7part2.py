57e938a620dea5a469e1098d37a1b3dd5ec3fba2
Adeola-Alabi <152799820+Adeola-Alabi@users.noreply.github.com>
2024-03-29 13:21:13 -0700

    Update Prog7part2.py
    commit

--- Prog7/Prog7part2.py
+++ Prog7/Prog7part2.py
@@ -1,9 +1,26 @@
 import os
 file_name = input("File Name: ")
-student_file = (file_name, "a")
+total_credits = 0
+final_grade = 0
 try:
-    
-        
-    
+    student_file = open(file_name, "r")
+    for x in student_file:
+        credits = x[-1]
+        total_credits = total_credits + credits
+        print(f"{a: <10}")
+        grade = x[-3]
+        if grade = A:
+            num_grade = credits * 4
+        if grade = B: 
+            num_grade = credits * 3
+        if grade = C:
+            num_grade = credits * 2
+        if grade = D:
+            num_grade = credits * 1
+        if grade = F:
+            num_grade = credits * 0
+        final_grade = final_grade + num_grade
+    GPA = final_grade / total_credits
+    print(GPA .f3)
 except:
     print("File not detected")
