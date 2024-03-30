a43e44caf04671403e9343b537aadb61d301eed8
Adeola-Alabi <Erron.smith2020@gmail.com>
2024-03-29 18:36:06 -0700

    working now

--- Prog7/Prog7part2.py
+++ Prog7/Prog7part2.py
@@ -2,25 +2,26 @@ import os
 file_name = input("File Name: ")
 total_credits = 0
 final_grade = 0
+num_grade = 0
 try:
     student_file = open(file_name, "r")
     for x in student_file:
-        credits = x[-1]
+        credits = int(x[-2])
         total_credits = total_credits + credits
-        print(f"{a: <10}")
-        grade = x[-3]
-        if grade = A:
+        print(f"{x: <100}")
+        grade = x[-4]
+        if grade == "A":
             num_grade = credits * 4
-        if grade = B: 
+        if grade == "B": 
             num_grade = credits * 3
-        if grade = C:
+        if grade == "C":
             num_grade = credits * 2
-        if grade = D:
+        if grade == 'D':
             num_grade = credits * 1
-        if grade = F:
+        if grade == 'F':
             num_grade = credits * 0
         final_grade = final_grade + num_grade
     GPA = final_grade / total_credits
-    print(GPA .f3)
+    print("%.2f" % GPA)
 except:
     print("File not detected")
